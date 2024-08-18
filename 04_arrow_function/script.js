7ae3fe0f26b6d75d575a728e6ed8b03081bdb2f3
Manish rawat <manish.rawat@essencesoftwares.com>
2024-08-18 09:24:44 +0530

    arrow function with this object

--- 04_arrow_function/script.js
+++ 04_arrow_function/script.js
@@ -12,12 +12,18 @@ const introduction = {
     role: "Frontend Developer",
     experience : "5 Years",
     show: function() {
-        setTimeout(function(){
-            // console.log(`my name is ${introduction.name} and i am a ${introduction.role}`) // this will still print values in console
-            // console.log(`my name is ${this.name} and i am a ${this.role}`) // in this case it will not print values in console
-            console.log(this); // print window object in console
+        const that = this //converting this into that to pass this as parent object
+        // console.log(this); // this will print whole object with key value pairs
+        // setTimeout(function(){ // for this normal function we need to convert our this(window object into object by using const that = this because it weill not take this from its parent like in normal situation)
+        //     // console.log(`my name is ${introduction.name} and i am a ${introduction.role}`) // this will still print values in console
+        //     // console.log(`my name is ${this.name} and i am a ${this.role}`) // in this case it will not print values in console
+        //     console.log(`my name is ${that.name} and i am a ${that.role}`) // in this case it will print values in console
+        //     // console.log(this); // print window object in console
+        // }, 3000)
+
+        setTimeout(() => { // in this case it will print values in console because of lexical scope of this
+            console.log(`my name is ${this.name} and i am a ${this.role}`)
         }, 3000)
-        // console.log(this);
     }
 }
 
