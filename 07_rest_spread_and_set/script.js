1619e16ab860a0ecb20842faad2a03d6886cc7fc
manish <manish.rawat@essencesoftwares.com>
2024-08-30 13:25:37 +0530

    knowing about set in js

--- 07_rest_spread_and_set/script.js
+++ 07_rest_spread_and_set/script.js
@@ -20,7 +20,24 @@
 
 // add(1,2,3,4,5,6,7,8)
 
+
 // rest and spread examples
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // spread example in array
@@ -44,10 +61,10 @@
 //     id3: 3,
 //     id4: 4
 // }
-// let obj3 = [
+// let obj3 = {
 //     ...obj1,
 //     ...obj2
-// ]
+// }
 // console.log(obj3)
 
 // spread example in object
@@ -81,3 +98,22 @@
 
 
 // rest example in object
+
+
+
+// set example in javascript
+
+const mySet = new Set();
+
+
+mySet.add(0);
+mySet.add(1);
+mySet.add(2);
+mySet.add(10);
+mySet.add(500);
+// mySet.delete(0); // it will delete 0 fom the set (object)
+
+// console.log(mySet.size); // it will print the lenght of the set (object)
+// console.log(mySet.has(1)); // it will check value which we pass in has(), if that find valsue , then it will print true / false accordingly
+
+console.log('total number of set is' +" "+ mySet.size +' '+ 'and do we have number 2 in the set:' +' '+ mySet.has(2));
