c3bb83cfa55b682457b9fd3f1e138a0f31b0bfd8
Manish rawat <manish.rawat@essencesoftwares.com>
2024-09-01 19:35:06 +0530

    knowing about aysnc and await ~~ basic

---
+++ 10_aysnc_await/script.js
@@ -0,0 +1,39 @@
+// async function introduction() {
+//     const responseData = await fetch('https://dummyjson.com/products')
+//     console.log(responseData);
+//     console.log('PLease load me after api');
+//     console.log('PLease load me after previous one');
+// }
+
+// function intro() {
+//     console.log("My name is manish");
+    
+// }
+
+// intro()
+// introduction()
+
+
+
+
+
+
+
+
+// function productList() {
+//     fetch('https://dummyjson.com/products')
+//     .then(res => res.json())
+//     .then(data => console.log(data))
+
+//     console.log("PLease load me after api");
+//     console.log("PLease load me after previous one");
+// }
+
+// function intro() {
+//     console.log("My name is manish");
+    
+// }
+
+
+// intro()
+// productList()
