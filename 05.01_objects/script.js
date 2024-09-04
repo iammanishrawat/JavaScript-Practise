bf7d51330608f85bd4cc3d61eb09c681228f9473
Manish rawat <manish.rawat@essencesoftwares.com>
2024-09-04 22:35:26 +0530

    part one of object knowledge

---
+++ 05.01_objects/script.js
@@ -0,0 +1,25 @@
+// const mySymbol = Symbol("Key1")
+
+// const obj1 = {
+//     "full Name": "Manish Rawat",
+//     age: 30,
+//     religion: "Hindu",
+//     nationality: "Indian",
+//     isAdult: true,
+//     [mySymbol]: "Key10000"
+// }
+
+// obj1.age = 28
+// Object.freeze(obj1) // now i am unable to reassign anything
+// obj1["full Name"] = "Manish Singh Rawat"
+
+// console.log(obj1.nationality); // dot notation
+// console.log(obj1["full Name"]); // bracket notation
+
+// obj1.introduction = function() {
+//     console.log(`Hello World, this is ${this["full Name"]}`);
+// }
+
+// obj1.introduction()
+
+// console.log(obj1);
