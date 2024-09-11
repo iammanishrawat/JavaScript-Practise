9ec673e8a6f01d9d17662220d891eba8a8a14c4d
manish <manish.rawat@essencesoftwares.com>
2024-09-11 18:02:07 +0530

    loop-3 loop addded

--- loop_practice/script.js
+++ loop_practice/script.js
@@ -1,183 +1,64 @@
-// for loop example
-
-// for (let i = 0; i <= 50; i++) {
+// for (let i = 0; i <= 100; i++) {
 //     console.log(i);
 // }
 
-// for (let i = 50; i >= 0 ; i--) {
-//     console.log(i);
-// }
-
-// let arrayOne = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-// for (let i = 0; i < arrayOne.length; i++) {
-//   console.log(arrayOne[i]);
-// }
-
-// for (let i = arrayOne.length - 1; i >= 0; i--) {
-//   console.log(arrayOne[i]);
-// }
-
-// let fruits = ["apple", "banana", "orange"];
-
-// for (let i = 0; i < fruits.length; i++) {
-//   console.log(fruits[i]);
-// }
-
-// for (let i = fruits.length - 1; i >= 0; i--) {
-//     console.log(fruits[i]);
-// }
-
-
-// while loop example
-
-// let i = 1;
-
-// while (i <= 5) {
-//   console.log(i);
-//   i++;
-// }
-
-
-// let i = 5;
-
-// while (i > 0) {
-//   console.log(i);
-//   i--;
-// }
-
-// let arrayOne = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-// let i = 0
-// while (i < arrayOne.length) {
-//     console.log(arrayOne[i]);
-//     i++
-// }
-
-// let i = arrayOne.length - 1
-// while (i >= 0) {
-//     console.log(arrayOne[i]);
-//     i--
-// }
-
-
-// do while examples
-
-
-// let i = 1;
-
-// do {
+// let i = 0;
+// while(i <= 100){
 //     console.log(i);
 //     i++
-// } while (i <= 5)
-
-// let i = 5;
-// do {
-//     console.log(i);
-//     i--
-// } while (i > 0)
-
-// let arrayOne = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// }
 
 // let i = 0;
 // do {
-//     console.log(arrayOne[i]);
+//     console.log(i);
 //     i++
-// } while (i < arrayOne.length)
-
-// let arrayOne = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-// let i = arrayOne.length - 1
-// do {
-//     console.log(arrayOne[i]);
-//     i--
-// } while (i >= 0)
-
-
-
-// debugging
-
-// let i = 5;
-
-// while (i < 5) {
-//   console.log(i);  // This will never execute because the condition is false.
-//   i++;
 // }
+// while (i <= 100)
 
 
-// let i = 5;
-
-// do {
-//   console.log(i);  // This will execute once, even though the condition is false.
-//   i++;
-// } while (i < 5);
-
-
-
-// array manipulation without using methods
-
-// let fruits = ["apple", "banana", "orange"]
-// i need to add mango at the last element in the existing array here (push)
-// fruits[fruits.length] = "mango"
-
-
-// i need to remove last element in the existing array here and change exiting array without last element (pop)
-// let lastFruit = fruits[fruits.length - 1]
-// fruits.length = fruits.length - 1
-// console.log(lastFruit);
-// console.log(fruits);
-
-
-// i need to add mango at the first element in the existing array here (unshift)
-// for (let i = fruits.length; i > 0; i--) {
-//     fruits[i] = fruits[i - 1]
-// }
-// fruits[0] = "mango";
-// console.log(fruits);
-
-// i need to remove first element in the existing array here and change exiting array without first element (shift)
-// let firstFruit = fruits[0]
-// for (let i = 0; i < fruits.length - 1; i++) {
-//     fruits[i] = fruits[i + 1]
-// }
-// fruits.length = fruits.length - 1
-// console.log(firstFruit);
-// console.log(fruits);
-
-// array manipulation without using methods
-
-
-// let sum = 0
-
-// for (let i = 1; i <= 100; i++) {
-//     sum += i
+// let sum = 0;
+// for (let i = 0; i <= 10; i++) {
+//     sum = sum + i
 // }
 // console.log(sum);
 
+// let sum = 0;
+// let i = 0;
+// while (i <= 10) {
+//     sum = sum + i
+//     i++
+// }
+// console.log(sum);
 
-// for (let i = 2; i <= 20; i+=2) {
-//     console.log(i);    
+// let sum = 0;
+// let i = 0
+
+// do {
+//     sum = sum + i
+//     i++    
+// } while (i <= 10)
+// console.log(sum);
+
+
+
+// for (let i = 1; i <= 10; i++) {
+//     if (i % 2 == 0) {
+//         console.log(i);
+//     }    
 // }
 
-// let factorialValue = 1
-// for (let i = 5; i >= 1; i--) {
-//     factorialValue *= i    
+// let i = 1
+// while (i <= 10) {
+//     if (i % 2 == 0) {
+//         console.log(i);
+//     }
+//     i++
 // }
 
-// console.log(factorialValue);
-
-
-
-// let myArrayOne = [1, 2, 3, 4, 5]
-
-// let newArrayOne = []
-// for (let i = 0; i <= myArrayOne.length - 1; i++){
-//     newArrayOne = myArrayOne.reverse()
-// }
-// console.log(newArrayOne);
-
-// let myArray = [1, 2, 3, 4, 5]
-// let newArray = []
-// for (let i = myArray.length - 1; i >= 0 ; i--) {
-//     newArray.push(myArray[i])
-// }
-// console.log(newArray);
+// let i = 1
+// do {
+//     if (i % 2 == 0) {
+//         console.log(i);
+//     }
+//     i++
+// } while (i <= 10);
