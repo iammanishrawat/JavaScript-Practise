df55f08bc63050ac1d4d6b37148ff00ddc305f24
manish <manish.rawat@essencesoftwares.com>
2024-09-11 20:09:09 +0530

    loop-3 loop addded revised

--- loop_practice/script.js
+++ loop_practice/script.js
@@ -1,3 +1,5 @@
+// print 0  to 100
+
 // for (let i = 0; i <= 100; i++) {
 //     console.log(i);
 // }
@@ -18,7 +20,7 @@
 
 
 
-
+// print sum of the numbers between 1 to 10
 
 // let sum = 0;
 // for (let i = 0; i <= 10; i++) {
@@ -46,7 +48,7 @@
 
 
 
-
+// print even numbers
 
 // for (let i = 1; i <= 10; i++) {
 //     if (i % 2 == 0) {
@@ -69,3 +71,42 @@
 //     }
 //     i++
 // } while (i <= 10);
+
+
+// print odd numbers
+
+// for (let i = 1; i <= 10; i++){
+//     if (i % 2 !== 0) {
+//         console.log(i);
+//     }
+// }
+
+// let i = 1
+// while (i <= 10) {
+//     if(i % 2 !== 0) {
+//         console.log(i)
+//     }
+//     i++
+// }
+
+// let i = 1
+// do {
+//     if (i % 2 !== 0) {
+//         console.log(i);
+//     }
+//     i++
+// } while (i <= 10)
+
+
+// print prime number
+
+for (let i = 1; i <= 10; i++) {
+    if (i % 1 == 0 && i % i == 0) {
+        // console.log(i);
+    }
+    for(let j = 2; j <= j - i; j++){
+        if (j % i !==0) {
+            console.log(i);
+        }
+    }
+}
