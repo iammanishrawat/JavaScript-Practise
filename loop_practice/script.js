adce63fb38d45535ddc4bda97e21446820b47c87
manish <manish.rawat@essencesoftwares.com>
2024-09-13 19:25:40 +0530

    prime number and factorial added

--- loop_practice/script.js
+++ loop_practice/script.js
@@ -1,4 +1,4 @@
-// print 0  to 100
+// print 0 to 100
 
 // for (let i = 0; i <= 100; i++) {
 //     console.log(i);
@@ -98,32 +98,91 @@
 // } while (i <= 10)
 
 
+
+
 // print prime number
 
-
-
-// for (let i = 1; i <= 10; i++) {
-//     if (i % 1 == 0 && i % i == 0) {
-//         // console.log(i);
-//     }
-//     for (let j = 1; j <= i; j++) {
-//         if (j == 2) {
-//             // console.log(j);
+// let primeNumber = []
+// for (let i = 2; i <= 100; i++) {
+//     let isDivisible = false;
+//     for (let j = 2; j <= i - 1; j++) {
+//         if (i % j == 0) {
+//             isDivisible = true
+//             break
 //         }
 //     }
-//     console.log(i);
-    
+//     if (!isDivisible) {
+//         primeNumber.push(i)
+//     }
 // }
 
-for (let i = 2; i <= 100; i++) {
-    flag = true;
-    for (let j = 2; j <= i - 1; j++) {
-        if (i % j == 0){
-            flag = false;
-            break;
-        }
-    }
-    if (flag) {
-        console.log(i);
-    }
-}
+// console.log(primeNumber);
+
+
+// let primeNumber = []
+// let i = 2;
+// while (i <= 100) {
+//     let j = 2
+//     let isDivisible = false
+//     while (j <= i - 1) {
+//         if (i % j == 0) {
+//             isDivisible = true
+//             break
+//         }
+//         j++
+//     }
+//     if (!isDivisible) {
+//         primeNumber.push(i)
+//     }
+//     i++
+// }
+
+// console.log(primeNumber);
+
+// let primeNumber = []
+// let i = 2
+// do {
+//     let j = 2
+//     let isDivisible = false
+//     do {
+//         if (i % j == 0) {
+//             isDivisible = true;
+//             break
+//         }
+//         j++
+//     } while (j <= i - 1);
+//     if (!isDivisible) {
+//         primeNumber.push(i)
+//     }
+//     i++
+// } while (i <= 100)
+
+// console.log(primeNumber)
+
+
+// print factorial of the numbers between 1 to 10
+
+// factors = 1
+// for (let i = 1; i <= 5; i++) {
+//     factors = factors * i
+// }
+
+// console.log(factors);
+
+// factors = 1
+// let i = 1
+// while (i <= 5) {
+//     factors = factors * i
+//     i++
+// }
+// console.log(factors)
+
+
+// factors = 1
+// let i = 1
+// do {
+//     factors = factors * i
+//     i++
+// } while (i <= 5);
+
+// console.log(factors);
